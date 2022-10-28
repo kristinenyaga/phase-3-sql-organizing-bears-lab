932abd0371619f49b9d3580012a495db59c1fce9
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-28 17:18:39 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT into bears(name,age,sex,color,temperament,alive) values ("Mr.Chocolate",2,"F","Brown","cute","TRUE");
+
+INSERT into bears(name,age,sex,color,temperament,alive) values ("Rowdy",6,"M","Brown","cute","TRUE");
+
+INSERT into bears(name,age,sex,color,temperament,alive) values ("Tabitha",2,"F","Brown","cute","FALSE");
+
+INSERT into bears(name,age,sex,color,temperament,alive) values ("Sergeant Brown",2,"M","Brown","cute","FALSE");
+
+INSERT into bears(name,age,sex,color,temperament,alive) values ("Melissa",9,"F","Brown","cute","TRUE");
+
+INSERT into bears(name,age,sex,color,temperament,alive) values ("Grinch",12,"M","Brown","cute","FALSE");
+
+INSERT into bears(name,age,sex,color,temperament,alive) values ("Wendy",1,"F","Brown","cute","TRUE");
+
+INSERT into bears(name,age,sex,color,temperament,alive) values (NULL,2,"F","Brown","cute","TRUE");
+
